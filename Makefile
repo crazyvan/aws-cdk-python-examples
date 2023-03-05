bad00d205550bbcc37b39863bf38c9bb3ba54063
crazyvan <crazyvan25@gmail.com>
2023-03-05 16:20:34 +0000

    first

---
+++ Makefile
@@ -0,0 +1,25 @@
+SHELL := /bin/bash
+AWS_ACCOUNT := 194086103172
+AWS_REGION := eu-west-2
+
+.PHONY: get-aws-credentials
+get-aws-credenials:
+	export AWS_PROFILE=people-data-system && \
+	npx aws-azure-login --no-sandbox --mode gui
+
+.PHONY: create-bootstrap-stack
+bootstrap:
+	cdk bootstrap aws://${AWS_ACCOUNT}/${AWS_REGION}
+
+.PHONY:	delete-bootstrap-stack
+delete-bootstrap-stack:
+	aws cloudformation delete-stack --stack-name CDKToolkit \
+	--region ${AWS_REGION}
+
+.PHONY: install-dev
+install-dev:
+	pip install -r requirements-dev.txt
+
+.PHONY: install
+install:
+	pip install -r requirements.txt
--- app.py
+++ app.py
@@ -3,26 +3,13 @@ import os
 
 import aws_cdk as cdk
 
-from aws_cdk_python_examples.aws_cdk_python_examples_stack import AwsCdkPythonExamplesStack
+from hello_cdk.hello_cdk_stack import HelloCdkStack
 
 
 app = cdk.App()
-AwsCdkPythonExamplesStack(app, "AwsCdkPythonExamplesStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+env_eu_west_2 = cdk.Environment(account="194086103172", region="eu-west-2")
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+HelloCdkStack(app, "HelloCdkStack", env=env_eu_west_2)
 
 app.synth()
